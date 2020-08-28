315a734bfb2c3fe116c94b11683c89521b8776d4
unknown <756617901@qq.com>
2020-08-28 17:13:46 +0800

    更新customerm尝试跨域访问

--- lib/model/customerm.dart
+++ lib/model/customerm.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:customer_manager/common/pool.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class Customerm {
   getCustomerlist(String needrow, String page) async {
-    String url =
-        pool.host + "/getcustomerlist?needrow=" + needrow + "&page=" + page;
+    String url = pool.host + "/getcustomerlist";
     /*if (Platform.isAndroid) {
       var httpClient = new HttpClient();
       try {
@@ -28,9 +28,13 @@ class Customerm {
     var client = http.Client();
     try {
       print(url);
-      var response = await client.post(url);
-      print("结束");
-      var json = response.body;
+      client.post(url,
+          body: 'needrow=' + needrow + "&page=" + page,
+          headers: {"Access-Control-Allow-Origin": "*"}).then((response) {
+        var json = jsonDecode(response.body);
+        print("结束");
+      });
+
       print(json);
     } on http.ClientException catch (ex) {
       print("获取客户列表错误:" + ex.message);
